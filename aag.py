cdf5d74860ecaf57f185cea91d57b63ed046f961
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2022-11-30 12:31:37 -0600

    Attempt at using Sage subgroup to keep everything as group objects, broken

--- aag.py
+++ aag.py
@@ -1,5 +1,6 @@
 from sage.all import *
 from sage.groups.group import Group
+from sage.sets.set import Set
 from typing import TypeVar, Generic
 import random
 
@@ -11,35 +12,35 @@ class AAGExchangeObject(Generic[T]):
     # subset of that group
 
     def __init__(self, group) -> None:
-        self.parent_group = group
+        self.G = group
 
-    _publicKey: list
+    _publicKey: T
 
     @property
     def publicKey(self):
         return self._publicKey
 
     @publicKey.setter
-    def publicKey(self, value) -> None:
+    def publicKey(self, value: T) -> None:
         self._publicKey = value
 
     @publicKey.getter
-    def publicKey(self):
+    def publicKey(self) -> T:
         return self._publicKey
 
     def generatePublicKey(self, length: int) -> None:
         assert length > 0
-        assert self.parent_group.order() >= length
+        assert self.G.order() >= length
 
         pk: set = set()
         while len(pk) < length:
-            pk.add(self.parent_group.random_element())
+            pk.add(self.G.random_element())
 
-        self._publicKey = list(pk)
+        self._publicKey = self.G.subgroup(pk)
 
-    _privateKey: list
+    _privateKey: T
 
-    def setPrivateKey(self, value) -> None:
+    def setPrivateKey(self, value: T) -> None:
         self._privateKey = value
     
     privateKey = property(None, setPrivateKey) # make private key unreadable
@@ -48,18 +49,40 @@ class AAGExchangeObject(Generic[T]):
         assert length > 0
         assert len(self.publicKey) >= length
 
-        self._privateKey = random.choices(list(self.publicKey))
+        pk: list = random.choices(list(self.publicKey), k=length)
+        self._privateKey = self.G.subgroup(pk)
 
     def __repr__(self) -> str:
         return f"Public Key: {self._publicKey} (Private Key: {self._privateKey})" # TODO: remove private key from repr
 
+    def transition(self, otherExchangeObject) -> list:
+        assert self._publicKey != None
+        assert otherExchangeObject.publicKey != None
+        assert self._privateKey != None
+
+        A: T = self._privateKey
+        print(A)
+        b: T = otherExchangeObject.publicKey
+        print(b)
+
+        # A^-1 * b_i * A for all b_i in B
+        Ai = self.G.subgroup([x.inverse() for x in A])
+        print(Ai)
+
+        transition = Ai * b * A
+
+        return transition
+        
     def deriveSharedKey(self, otherExchangeObject) -> list:
         assert self._publicKey != None
         assert otherExchangeObject.publicKey != None
         assert self._privateKey != None
 
         A = otherExchangeObject.publicKey
+        BiAB = otherExchangeObject.transition(self) # BiAB = B^-1 * a_i * B for all a_i in A
         B = self._privateKey
+
+        # key is Ai * BiAB
 
         sharedKey = [A[i].inverse() * B[i] * A[i] for i in range(len(B))]
 
