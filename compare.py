fee9e7a542c3bb479167e825d5f24a7784cf429a
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2022-12-16 13:44:19 -0600

    More graphs, add exp regression

--- compare.py
+++ compare.py
@@ -56,7 +56,7 @@ def timing(group_type, group_object, pk_length, sk_length):
 
 
 def generate(group_type, group, group_name, number_of_points, public_sizes, private_sizes):
-    with open('simulations/tricky.csv', 'a', newline='') as f:
+    with open('simulations/many.csv', 'a', newline='') as f:
         writer = csv.writer(f)
 
         for pub in tqdm(public_sizes, desc='Public',leave=False, position=1):
@@ -77,19 +77,35 @@ def main():
     #generate(PermutationGroup, SymmetricGroup(8), "S8", 15, [5], [6, 7])
     #generate(BraidGroup, BraidGroup(5), "Braid5", 3, [5], [1, 2, 3, 4, 5])
 
-    #generate(BraidGroup, BraidGroup(2), "Braid2", 3, [5], [1, 2, 3, 4, 5])
-    #generate(BraidGroup, BraidGroup(3), "Braid3", 3, [5], [1, 2, 3, 4, 5])
-    #generate(BraidGroup, BraidGroup(4), "Braid4", 3, [5], [1, 2, 3, 4, 5]) # dnf
+    #generate(QuaternionGroup, QuaternionGroup(), "Quaternion", 25, [1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
 
-    #generate(QuaternionGroup, QuaternionGroup(), "Quaternion", 50, [5], [1, 2, 3, 4, 5])
+    #generate(HeisenbergGroup, HeisenbergGroup(n=5, R=Integer(sys.maxsize)), "Heisenberg5", 25, [4,5], [1, 2, 3, 4, 5, 6])
 
-    #generate(HeisenbergGroup, HeisenbergGroup(n=5, R=Integer(sys.maxsize)), "Heisenberg5", 15, [5], [1, 2, 3, 4, 5])
-    #generate(HeisenbergGroup, HeisenbergGroup(n=5, R=Integer(sys.maxsize)), "Heisenberg5", 5, [5], [5])
+    #generate(CubeGroup, CubeGroup(), "Rubik", 25, [1,2,3,4,5], [1, 2, 3, 4, 5])
 
     #generate(PermutationGroup, SymmetricGroup(16), "S16", 5, [5], [1, 2, 3, 4, 5, 6, 7])
     #generate(PermutationGroup, SymmetricGroup(16), "S16", 10, [5], [8])
-    generate(PermutationGroup, SymmetricGroup(16), "S16", 20, [2], [6])
-    generate(PermutationGroup, SymmetricGroup(16), "S16", 20, [8], [2])
+
+    #generate(PermutationGroup, SymmetricGroup(16), "S16", 100, [1,2,3,4,5], [1,2,3,4,5])
+    #generate(PermutationGroup, SymmetricGroup(16), "S16", 25, [5], [6,7,8,9,10,11,12])
+
+    #generate(BraidGroup, BraidGroup(2), "Braid2", 3, [1,2,3,4,5], [1, 2, 3, 4, 5])
+    #generate(BraidGroup, BraidGroup(3), "Braid3", 3, [1,2,3,4,5], [1, 2, 3, 4, 5])
+    generate(BraidGroup, BraidGroup(3), "Braid3", 15, [5], [1,2,3])
+
+    #generate(HeisenbergGroup, HeisenbergGroup(n=5, R=Integer(sys.maxsize)), "Heisenberg5", 25, [6,7], [1, 2, 3, 4, 5, 6])
+
+    #generate(PermutationGroup, SymmetricGroup(32), "S32", 10, [5], [1,2,3,4,5,6])
+    #generate(PermutationGroup, SymmetricGroup(64), "S64", 10, [5], [1,2,3,4,5,6])
+
+    #generate(BraidGroup, BraidGroup(4), "Braid4", 3, [1,2,3,4], [1,2,3,4])
+
+    #generate(BraidGroup, BraidGroup(3), "Braid3", 3, [5], [6])
+
+    #generate(PermutationGroup, SymmetricGroup(16), "S16", 3, [1,2,3,4,5,6,7,8,9,10], [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
+    #generate(BraidGroup, BraidGroup(3), "Braid3", 3, [5], [7,8,9,10])
+    pass
 
 if __name__ == "__main__":
     main()
